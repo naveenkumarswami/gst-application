aec6d075f67a98264884dae1ba2a3e550538251f
naveenkumarswami <nsw.axelor@gmail.com>
2019-07-19 14:10:20 +0530

    modify the invoice view

--- modules/axelor-gst/src/main/java/com/axelor/gst/web/GstController.java
+++ modules/axelor-gst/src/main/java/com/axelor/gst/web/GstController.java
@@ -2,6 +2,7 @@ package com.axelor.gst.web;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.DoubleStream;
 import javax.inject.Inject;
 import com.axelor.gst.db.Address;
 import com.axelor.gst.db.Company;
@@ -33,51 +34,81 @@ public class GstController {
 
     Invoice invoice = request.getContext().asType(Invoice.class);
     Contact contact = service.getContact(invoice);
+
+    
+    // 2nd method to find the address
+//    System.out.println(
+//        "invoice add "
+//            + invoice
+//                .getParty()
+//                .getAddressList()
+//                .stream()
+//                .filter(a -> a.getType().equals("invoice") || a.getType().equals("default"))
+//                .findFirst());
+
     Address invocieAddress = service.getInvoiceAddress(invoice);
     Address shippingAddress = service.getShippingAddress(invoice);
 
     request.getContext().put("primaryContact", contact);
     request.getContext().put("defalultinvoiceAddress", invocieAddress);
     request.getContext().put("defalultshippingAddress", shippingAddress);
-    
-    System.err.println(invocieAddress );
+
+    System.err.println(invocieAddress);
     System.err.println(shippingAddress);
   }
-  
-  public void getCompanyAndInvoiceState(ActionRequest request, ActionResponse response)
-  {
-    
+
+  public void getCompanyAndInvoiceState(ActionRequest request, ActionResponse response) {
+
     InvoiceLine invoiceLine = request.getContext().asType(InvoiceLine.class);
     Invoice invoice = request.getContext().getParent().asType(Invoice.class);
-    
+
     BigDecimal setIGST = service.getIGST(invoice, invoiceLine);
     BigDecimal setSGSTAndCGST = service.getSGSTandCGST(invoice, invoiceLine);
-    
+
     /*response.setValue("igst", setIGST);
     response.setValue("sgst", setSGSTAndCGST);
     response.setValue("cgst", setSGSTAndCGST);*/
     request.getContext().put("igstvalue", setIGST);
     request.getContext().put("sGSTandCgst", setSGSTAndCGST);
-    
   }
-  
-  public void getDefalutCompany(ActionRequest request, ActionResponse response)
-  {
+
+  public void getDefalutCompany(ActionRequest request, ActionResponse response) {
     Invoice invoice = request.getContext().asType(Invoice.class);
     Company company = service.setDefalutComany(invoice);
-    System.out.println(company ); 
-    response.setValue("company",company);
-    
+    System.out.println(company);
+    response.setValue("company", company);
   }
-  
-  public void getCompanySta(ActionRequest request, ActionResponse response)
-  {
+
+  public void getCompanySta(ActionRequest request, ActionResponse response) {
     Invoice invoice = request.getContext().asType(Invoice.class);
-    
-    request.getContext().put("companyState", invoice.getCompany().getAddress().getState()); 
+    request.getContext().put("companyState", invoice.getCompany().getAddress().getState());
   }
-  
-  
-  
-  
+
+  public void setAllValues(ActionRequest request, ActionResponse response) {
+    Invoice invoice = request.getContext().asType(Invoice.class);
+
+    List<InvoiceLine> invoiceLine = invoice.getInvoiceItemsList();
+
+    double netAmount =
+        invoiceLine
+            .stream()
+            .mapToDouble(getNetAmount -> getNetAmount.getNetAmount().doubleValue())
+            .sum();
+    double NetIgst =
+        invoiceLine.stream().mapToDouble(getIgst -> getIgst.getIgst().doubleValue()).sum();
+    double netCgst =
+        invoiceLine.stream().mapToDouble(getCgst -> getCgst.getCgst().doubleValue()).sum();
+    double netSgst =
+        invoiceLine.stream().mapToDouble(getSgst -> getSgst.getSgst().doubleValue()).sum();
+    
+    double grossAmount = invoiceLine.stream().mapToDouble(getGrossAmount -> getGrossAmount.getGrossAmount().doubleValue()).sum();
+    
+    response.setValue("netAmount", BigDecimal.valueOf(netAmount));
+    response.setValue("netIgst", BigDecimal.valueOf(NetIgst));
+    response.setValue("netCsgt", BigDecimal.valueOf(netCgst));
+    response.setValue("netSgst", BigDecimal.valueOf(netSgst));
+    response.setValue("grossAmount", BigDecimal.valueOf(grossAmount));
+    
+    
+  }
 }
